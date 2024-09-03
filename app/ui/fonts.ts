d8ebf6996a885873c61458f9328c048fa2bb47ef
Kamlo Stephane <stephanekamlo23@gmail.com>
2024-09-03 12:24:03 +0100

    removing fake imports

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+
+import { Poppins, Inter, Lusitana } from 'next/font/google';
+ 
+
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['300', '400', '600', '700', '800'],
+  });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+  });
